93b82413a6d4126aa861b0fecf685a7b1e1f3754
m.heiss <unconfigured@null.spigotmc.org>
2022-04-15 14:06:15 +0200

    umbau in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-ignore
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+// @ts-ignore
+import Dashboard from './ts/dashboard'
+import MainPage from './ts/mainpage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Route path="/" exact component={MainPage}/>
+            <Route path="/Dashboard" component={Dashboard}/>
+        </Router>
+
+
+    );
 }
 
 export default App;
+
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.tsx</code> and save to reload.
+//     </p>
+//     <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+//   </header>
+// </div>
